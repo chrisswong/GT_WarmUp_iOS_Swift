fd8d587c178d2a134426b9ca24e81d930762e14c
Chris Wong <chris.wong@gtomato.com>
2015-04-01 18:08:18 +0800

    init project

---
+++ GT_WarmUp_iOS_Swift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GT_WarmUp_iOS_Swift
+//
+//  Created by Chris on 1/4/15.
+//  Copyright (c) 2015 Green Tomato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
