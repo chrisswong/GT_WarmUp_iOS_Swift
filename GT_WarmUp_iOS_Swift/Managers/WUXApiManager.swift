09bf472b434a9b35c448f19551be33613d3789c0
Chris Wong <chris.wong@gtomato.com>
2015-04-02 13:10:10 +0800

    Completed Profile ViewController

--- GT_WarmUp_iOS_Swift/Managers/WUXApiManager.swift
+++ GT_WarmUp_iOS_Swift/Managers/WUXApiManager.swift
@@ -11,12 +11,81 @@ import Alamofire
 
 class WUXApiManager {
     
-    typealias ProfileCallback = (NSError?, AnyObject?) -> (Void)
-    class func retrieveProfile(callback: ProfileCallback) {
+    typealias WUXAPICallback = (NSError?, AnyObject?) -> (Void)
+    
+    typealias PropertyList = AnyObject
+    
+    class func retrieveProfile(callback: WUXAPICallback) {
         
         Alamofire.request(.GET, "http://api.randomuser.me/")
             .response { (request, response, data, error) in
-                
+                var propertyListResponse: PropertyList?
+
+                if data != nil {
+                    propertyListResponse = NSJSONSerialization.JSONObjectWithData(
+                        data as NSData,
+                        options: NSJSONReadingOptions.MutableLeaves,
+                        error: nil
+                    )
+                    
+                    if propertyListResponse != nil {
+                        if let dictionary = propertyListResponse as? NSDictionary {
+                            if let profileList = dictionary["results"] as? NSArray {
+                                if let profileDict = profileList[0] as? NSDictionary {
+                                    if let userDict = profileDict["user"] as? NSDictionary {
+                                        var profile:WUXProfile = WUXProfile()
+                                        
+                                        if let userEmail = userDict["email"] as? String {
+                                            profile.email = userEmail
+                                        }
+                                        
+                                        if let userName = userDict["username"] as? String {
+                                            profile.userName = userName
+                                        }
+                                        
+                                        if let userPassword = userDict["password"] as? String {
+                                            profile.password = userPassword
+                                        }
+                                        
+                                        if let userPhoneNo = userDict["phone"] as? String {
+                                            profile.phoneNo = userPhoneNo
+                                        }
+                                        
+                                        if let userSnsNo = userDict["NINO"] as? String {
+                                            profile.snsNo = userSnsNo
+                                        }
+                                        
+                                        if let profilePicDict = userDict["picture"] as? NSDictionary {
+                                            
+                                            if let profileThumbnailUrlString = profilePicDict["thumbnail"] as? String {
+                                                profile.pictureUrlString = profileThumbnailUrlString
+                                            }
+                                        }
+                                        
+                                        callback(nil , profile)
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        
+                        var errorDict = [NSLocalizedDescriptionKey:"JSON could not parse."]
+                        var jsonCouldNotParseError = NSError(domain: "com.gtomato.enterprise.ios.GT-Warm-UP", code: 999, userInfo: errorDict)
+                        callback(jsonCouldNotParseError , nil)
+                    }
+                    
+                } else {
+                    
+                    var errorDict = [NSLocalizedDescriptionKey:"Empty JSON Response"]
+                    var jsonCouldNotParseError = NSError(domain: "com.gtomato.enterprise.ios.GT-Warm-UP", code: 998, userInfo: errorDict)
+                    callback(jsonCouldNotParseError , nil)
+                }
         }
     }
+    
+    private func log(whatToLog: AnyObject) {
+        debugPrintln("TwitterRequest: \(whatToLog)")
+    }
 }
+
+
--- GT_WarmUp_iOS_Swift/Profile/WUXProfileViewController.swift
+++ GT_WarmUp_iOS_Swift/Profile/WUXProfileViewController.swift
@@ -10,10 +10,77 @@ import UIKit
 
 class WUXProfileViewController: UIViewController {
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        WUXApiManager.retrieveProfile { (error, response) -> (Void) in
-            
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneNoLabel: UILabel!
+    @IBOutlet weak var snsNoLabel: UILabel!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    var currentProfile:WUXProfile? = nil {
+        didSet {
+            updateUI()
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        WUXApiManager.retrieveProfile { (error, response) -> (Void) in
+            
+            if error == nil {
+                if let profile = response as? WUXProfile {
+                    self.currentProfile = profile
+                }
+            } else {
+                
+            }
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+        self.profileImageView.layer.masksToBounds = true
+    }
+    
+    func updateUI() {
+        
+        if self.currentProfile != nil {
+            
+            self.userNameLabel.text = self.currentProfile?.userName
+            self.passwordLabel.text = self.currentProfile?.password
+            self.emailLabel.text    = self.currentProfile?.email
+            self.phoneNoLabel.text  = self.currentProfile?.phoneNo
+            self.snsNoLabel.text    = self.currentProfile?.snsNo
+            
+            //let's do it in native way.
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
+            
+            if let imageUrlString = self.currentProfile?.pictureUrlString {
+                if let imageUrl = NSURL(string: imageUrlString) {
+                    dispatch_async(dispatch_get_global_queue(qos, 0)) {
+                        
+                        let imageData = NSData(contentsOfURL: imageUrl)
+                        
+                        dispatch_async(dispatch_get_main_queue()) {
+                            
+                            if imageData != nil {
+                                self.profileImageView.image = UIImage(data: imageData!)
+                            } else {
+                                self.profileImageView.image = nil;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
