ce71457258868bca9048348f31627f3c80588f67
Chris Wong <chris.wong@gtomato.com>
2015-04-21 12:45:07 +0800

    ADDED scrollView to PhotoDetailViewController  ADDED Segue Handling for AlbumViewController

--- GT_WarmUp_iOS_Swift/Album/WUXAlbumCollectionViewController.swift
+++ GT_WarmUp_iOS_Swift/Album/WUXAlbumCollectionViewController.swift
@@ -11,11 +11,17 @@ import UIKit
 class WUXAlbumCollectionViewController: WUXBaseViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var collectionViewSelectedIndexPath:NSIndexPath?
+    
     var photoList:[WUXPhoto] = [WUXPhoto]() {
         didSet {
             self.collectionView.reloadData()
         }
     }
+    
+    //MARK: -
+    //MARK: LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,51 @@ class WUXAlbumCollectionViewController: WUXBaseViewController, UICollectionViewD
         super.viewWillAppear(animated)
         fetchPhotos()
     }
+    
+    //MARK: -
+    //MARK: CollectionView 
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photoList.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = self.collectionView?.dequeueReusableCellWithReuseIdentifier(Constants.Storyboard.AlbumCollectionViewCellIdentifier, forIndexPath: indexPath) as! WUXAlbumCollectionViewCell
+        
+        let photo = self.photoList[indexPath.row]
+        weak var weakSelf = self
+        cell.configureCell(photo, addButtonActionHandler: { (sender) -> (Void) in
+            var indexPath:NSIndexPath = collectionView.indexPathForCell(sender)!
+            weakSelf?.handleFavourite(indexPath.item)
+        })
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.collectionViewSelectedIndexPath = indexPath
+        self.performSegueWithIdentifier(Constants.Storyboard.AlbumShowDetailSegueIdentifier , sender: nil)
+    }
+    
+    //MARK: -
+    //MARK: Storyboard
+    
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == Constants.Storyboard.AlbumShowDetailSegueIdentifier {
+            if let photoDetailViewController = segue.destinationViewController as? WUXPhotoDetailViewController {
+                if self.collectionViewSelectedIndexPath != nil {
+                    let selectedPhoto = self.photoList[self.collectionViewSelectedIndexPath!.row]
+                    photoDetailViewController.currentPhoto = selectedPhoto
+                }
+            }
+        }
+
+    }
+    
+    //MARK: -
+    //MARK: Helper Methods
     
     func fetchPhotos() {
         
@@ -47,24 +98,6 @@ class WUXAlbumCollectionViewController: WUXBaseViewController, UICollectionViewD
         }
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.photoList.count
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cell = self.collectionView?.dequeueReusableCellWithReuseIdentifier(Constants.Storyboard.AlbumCollectionViewCellIdentifier, forIndexPath: indexPath) as! WUXAlbumCollectionViewCell
-        
-        let photo = self.photoList[indexPath.row]
-        weak var weakSelf = self
-        cell.configureCell(photo, addButtonActionHandler: { (sender) -> (Void) in
-            var indexPath:NSIndexPath = collectionView.indexPathForCell(sender)!
-            weakSelf?.handleFavourite(indexPath.item)
-        })
-        
-        return cell
-        
-    }
     
     func handleFavourite(index: Int) {
         let photo = self.photoList[index]
